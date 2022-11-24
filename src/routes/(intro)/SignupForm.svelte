1ff9c4c696dee92c962b6c3e76f53061b562572f
DSoloha <dsoloha@live.com>
2022-11-24 01:02:20 -0500

    Added basic landing page

---
+++ src/routes/(intro)/SignupForm.svelte
@@ -0,0 +1,21 @@
+<script>
+	import Button from '$components/Button.svelte';
+	import Input from '$components/Input.svelte';
+</script>
+
+<form>
+	<Input name="username" placeholder="Username" />
+	<Input name="password" placeholder="Password" type="password" />
+	<Button>Sign up</Button>
+</form>
+
+<style>
+	@media only screen and (max-width: 600px) {
+		form {
+			display: flex;
+			flex-direction: column;
+			justify-content: center;
+			align-items: center;
+		}
+	}
+</style>
