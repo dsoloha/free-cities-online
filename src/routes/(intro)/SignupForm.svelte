7d0639ebffd4bd16a3821d935db322f9f0cd6705
DSoloha <dsoloha@live.com>
2022-11-25 23:57:28 -0500

    Minor styling tweaks

--- src/routes/(intro)/SignupForm.svelte
+++ src/routes/(intro)/SignupForm.svelte
@@ -15,8 +15,8 @@
 
 <form>
 	<div class="center">
-		<Input name="email" placeholder="email" type="email" />
-		<Input name="password" placeholder="password" type="password" />
+		<Input name="username" placeholder="Username" />
+		<Input name="password" placeholder="Password" type="password" />
 		<Button focus={true}>Go -></Button>
 	</div>
 </form>
