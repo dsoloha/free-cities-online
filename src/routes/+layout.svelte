557191f111c2239b9e9e304b75a88b7e9126d101
DSoloha <dsoloha@live.com>
2022-12-02 11:37:50 -0500

    Combined layout files into single file

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,17 +1,21 @@
 <script>
-	import Header from '$components/Header.svelte'
+	import Header from '$components/Header.svelte';
 	import Footer from '$components/Footer.svelte'
 	import '$lib/styles.css'
+
+	let user = false // currently disabled until I get auth working
 </script>
 
 <div class="app">
-	<Header />
-
+	{#if user}
+		<Header />
+	{/if}
 	<main>
 		<slot />
 	</main>
-
-	<Footer />
+	{#if user}
+		<Footer />
+	{/if}
 </div>
 
 <style>
