2f2a96f43b04667c2c6a1ace7db948b4c5e0dfaa
DSoloha <dsoloha@live.com>
2022-12-05 23:23:28 -0500

    Formatting

--- src/routes/(app)/locations/+page.svelte
+++ src/routes/(app)/locations/+page.svelte
@@ -1,18 +1,18 @@
 <div class="container mx-auto p-8 space-y-8">
 	<h1>Locations</h1>
 	<h2>Gene lab</h2>
-	<hr>
+	<hr />
 	<h2>Pharmaceutical fabricator</h2>
-	<hr>
+	<hr />
 	<h2>Organ farm</h2>
-	<hr>
+	<hr />
 	<h2>Implant manufactory</h2>
-	<hr>
+	<hr />
 	<h2>Wardrobe</h2>
-	<hr>
+	<hr />
 	<h2>Toy shop</h2>
-	<hr>
+	<hr />
 	<h2>Media studio</h2>
-	<hr>
+	<hr />
 	<h2>Black market</h2>
 </div>
