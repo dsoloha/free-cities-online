a694c9450bd0d64afa98b177206b010d99c37347
DSoloha <dsoloha@live.com>
2022-12-06 17:19:58 -0500

    Moved styles.css into css folder

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,7 +1,7 @@
 <script>
 	import Header from '$components/Header.svelte'
 	import Footer from '$components/Footer.svelte'
-	import '$lib/styles.css'
+	import '$lib/css/styles.css'
 	import '$lib/css/fonts.css'
 
 	let user = false // currently disabled until I get auth working
