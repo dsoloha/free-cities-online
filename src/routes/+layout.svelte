db379ce04468d1f94536cd2e9299bebe5268a40c
DSoloha <dsoloha@live.com>
2022-12-06 20:34:25 -0500

    Formatting

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -9,23 +9,25 @@
 </script>
 
 <AppShell slotSidebarLeft="bg-surface-500/5 w-56 p-4">
-	<!-- Header -->
 	<svelte:fragment slot="header">
-		<!-- Insert the App Bar: -->
 		<AppBar>
 			<svelte:fragment slot="lead">
 				<h3>fcOS</h3>
 			</svelte:fragment>
+
 			<svelte:fragment slot="trail">
 				<LightSwitch />
-				<a class="btn btn-sm" href="https://github.com/" target="_blank" rel="noreferrer">GitHub</a>
+				<a
+					class="btn btn-sm"
+					href="https://github.com/dsoloha/free-cities-online"
+					target="_blank"
+					rel="noreferrer">GitHub</a
+				>
 			</svelte:fragment>
 		</AppBar>
-		<!-- --- -->
 	</svelte:fragment>
-	<!-- Sidebar -->
+
 	<svelte:fragment slot="sidebarLeft">
-		<!-- Insert the list: -->
 		<nav class="list-nav">
 			<ul>
 				<li><a href="/">home</a></li>
@@ -38,27 +40,6 @@
 				<li><a href="/tools">tools</a></li>
 			</ul>
 		</nav>
-		<!-- --- -->
 	</svelte:fragment>
-	<!-- Page Content Slot -->
 	<slot />
 </AppShell>
-
-<style>
-	.app {
-		display: flex;
-		flex-direction: column;
-		min-height: 100vh;
-	}
-
-	main {
-		flex: 1;
-		display: flex;
-		flex-direction: column;
-		padding: 1rem;
-		width: 100%;
-		max-width: 64rem;
-		margin: 0 auto;
-		box-sizing: border-box;
-	}
-</style>
